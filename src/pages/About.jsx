ada0151bf49af8a681c68564b17f2ff90b7f4709
Michael Mburu <ichangaimichael@yahoo.com>
2022-08-13 10:27:00 +0300

    - Initialized Github Finder Project
    - Setup Tailwind and Daisy UI
    
    - Added Navbar & Footer conmponent
    
    - Added Home, About and NotFound pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaTwitter} from 'react-icons/fa'
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">
+        <p className='mb-4 text-2xl font-light'>
+          This is an app to search GitHub profiles and see profile details. 
+        </p>
+        <p className='text-lg text-gray-400'>
+          Version <span className='text-white'>1.0.0</span>
+        </p>
+        <p className='text-lg text-gray-400'>
+          FrontEnd Engineer:
+        
+          <a className='text-white mx-2' href='https://twitter.com/codercety'>
+            Michael Mburu   
+            <FaTwitter className='inline pr-2 mx-2 text-3xl' />
+          </a>
+         
+        </p>
+      </h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const 
+h1 = () => {
+  return (
+    <div>
+      <h1 className='text-6xl'> Welcome Home</h1>
+    </div>
+  )
+}
+
+export default 
+h1
