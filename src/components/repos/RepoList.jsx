56b6c66cafd5227e0554432003ecf79c37208f5c
Michael Mburu <ichangaimichael@yahoo.com>
2022-12-08 21:30:55 +0300

    - Fixed RepoList title showing when null

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -7,9 +7,9 @@ const RepoList = ({repos}) => {
         
         <div className="rounded-lg shadow-lg card bg-base-100">
             <div className="card-body">
-                <h2 className="text-3xl my-4 font-bold card-title">
+                {repos ?? <h2 className="text-3xl my-4 font-bold card-title">
                     Latest Repositories
-                </h2>
+                </h2>}
                 {repos.map((repo) => (
                     <RepoItem key ={repo.id} repo={repo}/>
                 ))}
